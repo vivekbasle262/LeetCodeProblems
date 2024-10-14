61691555e9dbd526345024e980c23ebe487db65f
Vivek_Rajput <118098317+vivekbasle262@users.noreply.github.com>
2024-10-14 21:58:58 +0530

    Time: 156 ms (35.07%), Space: 63.2 MB (37.91%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -1,5 +1,20 @@
+import java.lang.Math;
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        
+        PriorityQueue<Integer>p=new PriorityQueue<>(Collections.reverseOrder());
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            p.offer(nums[i]);
+        }
+        long score=0;
+        while(k>0)
+        {
+            int val=p.poll();
+            score+=val;
+            p.offer((int) Math.ceil(val/3.0));
+            k--;
+        }
+        return score;
     }
 }
