8b7505f6bd393d8bcce6e57455f177dfa0090eeb
Vivek_Rajput <118098317+vivekbasle262@users.noreply.github.com>
2024-10-15 20:11:55 +0530

    Time: 7 ms (100%), Space: 45.2 MB (70.97%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,5 +1,17 @@
 class Solution {
     public long minimumSteps(String s) {
+       long ans=0,c=0;
+       for(int i=0;i<s.length();i++)
+       {
+        if(s.charAt(i)=='1')
+        {
+            c++;
+        }
+        else{
+            ans+=c;
+        }
+       }
+     return ans;
         
     }
 }
